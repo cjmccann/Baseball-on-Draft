6cba78f3dda8a0d361e6d7dae3132f2687f8d52f
Connor J McCann <connor.j.mccann@gmail.com>
2017-03-25 00:46:37 -0700

    adding flag to see if Puma is defined. to ensure on server restart we resave player records with new stats added to jsons

--- config/initializers/z_seed_players.rb
+++ config/initializers/z_seed_players.rb
@@ -1,4 +1,4 @@
-if ActiveRecord::Base.connection.tables.include?('players') && !defined?(::Rake) && defined?(Rails::Server)
+if ActiveRecord::Base.connection.tables.include?('players') && !defined?(::Rake) && (defined?(Rails::Server) || defined?(::Puma))
   Dir[File.join(Rails.root, 'lib', 'stats', '**', '*.rb')].each { |f| require_dependency f }
 
   parser = ProjectionParser.new
